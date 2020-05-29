fdd7ecfb22169e20f90d67ed14b4d987057db746
YoungDriverOfTech <xiaoleliang045@gmail.com>
2020-05-29 15:47:12 +0900

    webpack自动生成html

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -6,5 +7,10 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '无极麻辣油',
+        // Load a custom template (lodash by default)
+        template: './src/index.html'
+    })]
 };
